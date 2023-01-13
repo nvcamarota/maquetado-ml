333d2411ade9f08be84e0e1ec8aa03d885c44d2d
Nazarena <nvcamarota@gmail.com>
2023-01-13 18:43:40 -0300

    Agrega animación en descripción del producto y camión | Modifica footer

--- app.js
+++ app.js
@@ -27,12 +27,12 @@ app.get('/registration', (req, res) => res.sendFile(path.resolve(__dirname, 'vie
 app.get('/login', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'users', 'login.html')));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
